bf4b32f74a36a62712f29254130e19ccd2a2c952
JcbSm <49797894+JcbSm@users.noreply.github.com>
2022-12-30 23:34:23 +0000

    feat: file helpers

---
+++ src/lib/helpers/files.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+export type File = {
+    __filename: string;
+    __dirname: string;
+}
+
+/**
+ * Converts import.meta.url to standard __dirname __filename format.
+ * @param url import.meta.url
+ * @returns File
+ */
+export function parseURL(url: string): File {
+
+    const __filename = fileURLToPath(url);
+    const __dirname = path.dirname(__filename);
+
+    return { __filename, __dirname }
+
+}
+
+/**
+ * Gets the name of the directory of a File
+ * @param file File to parse
+ * @returns Directory name
+ */
+export function dirname(file: File) {
+    return file.__dirname.split(path.sep).pop();
+}
+
+/**
+ * Get the name of the directory for the current file.
+ * @param url 
+ * @returns Directory name
+ */
+export function getDirname(url: string) {
+    return dirname(parseURL(url))
+}
