6e8a95da0502476ad1ffc86f586e0bd7ed2fd80d
JcbSm <jacob.ted.seaman@gmail.com>
2023-02-21 12:20:11 +0000

    fix: Remove riby QOTD

--- dist/commands/guilds/1061726301783867472/ribyquote.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismCommand } from "#structs/PrismCommand";
-import type { ChatInputCommand } from "@sapphire/framework";
-export declare class RibyCommand extends PrismCommand {
-    registerApplicationCommands(registry: ChatInputCommand.Registry): void;
-    chatInputRun(interaction: PrismCommand.ChatInputInteraction): Promise<void>;
-    private drawRiby;
-}
-//# sourceMappingURL=ribyquote.d.ts.map
