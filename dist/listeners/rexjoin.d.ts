0c5ea958da978be056b71c5b283074fdc512919f
JcbSm <49797894+JcbSm@users.noreply.github.com>
2023-05-29 12:07:58 +0100

    feat: removed anti-rex

--- dist/listeners/rexjoin.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismListener } from "#structs/PrismListener";
-import type { GuildMember } from "discord.js";
-export default class extends PrismListener {
-    run(member: GuildMember): Promise<void>;
-}
-//# sourceMappingURL=rexjoin.d.ts.map
